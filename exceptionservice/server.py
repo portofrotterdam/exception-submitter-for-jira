a25495475b68b64f8f24f7f850db5719b36097dc
Miel Donkers <miel.donkers@codecentric.nl>
2016-09-27 21:24:29 +0200

    Match also on exception throw location

--- exceptionservice/server.py
+++ exceptionservice/server.py
@@ -1,5 +1,6 @@
 import logging
 import io
+import re
 from copy import deepcopy
 from difflib import SequenceMatcher
 from urllib.parse import urljoin
@@ -22,6 +23,8 @@ _JIRA_URI_CREATE = urljoin(JIRA_URI, '/rest/api/latest/issue')
 _JIRA_USER_PASSWD = (JIRA_USER, JIRA_PASSWD)
 _JIRA_FIELDS = ['id', 'key', 'created', 'status', 'labels', 'summary', 'description']
 _CONTENT_JSON_HEADER = {'Content-Type': 'application/json'}
+
+REGEX_CAUSED_BY = re.compile(r'\W*caused\W+by', re.IGNORECASE)
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -60,8 +63,8 @@ def add_jira_exception(json_data):
 
 
 def determine_if_duplicate(json_data):
-    exception = get_summary_from_message(json_data)
-    query = {'jql': 'project=HAMISTIRF&issuetype=Bevinding&summary ~ ' + exception, 'fields': _JIRA_FIELDS}
+    exception_summary = get_summary_from_message(json_data)
+    query = {'jql': 'project=HAMISTIRF&issuetype=Bevinding&summary ~ ' + exception_summary, 'fields': _JIRA_FIELDS}
     resp = requests.post(_JIRA_URI_SEARCH,
                          json=query,
                          headers=_CONTENT_JSON_HEADER,
@@ -70,15 +73,15 @@ def determine_if_duplicate(json_data):
     if resp.status_code != 200:
         return True, 'Could not query Jira issues, cancel processing issue. Resp code; {}'.format(resp.status_code)
 
-    # log.info('QUERY result: \n' + json.dumps(resp.json()) + '\n\n')
+    new_stacktrace = get_stacktrace_from_message(json_data)
     for issue in resp.json()['issues']:
         issue_stacktrace = get_stacktrace_from_issue(issue)
         s = SequenceMatcher(lambda x: x == ' ' or x == '\n' or x == '\t',
-                            get_stacktrace_from_message(json_data),
+                            new_stacktrace,
                             issue_stacktrace)
 
         match_ratio = s.ratio() if s.real_quick_ratio() > 0.6 else 0
-        if match_ratio > 0.95:
+        if match_ratio > 0.95 and matches_exception_throw_location(new_stacktrace, issue_stacktrace):
             log.info('\nMatch ratio: {} for stacktrace:\n{}'.format(match_ratio, issue_stacktrace))
             return True, issue['key']
 
@@ -87,11 +90,30 @@ def determine_if_duplicate(json_data):
 
 def get_stacktrace_from_issue(issue):
     description = issue['fields']['description']
-    descr_blocks = description.split('{noformat}')
-    if len(descr_blocks) >= 3:
-        return descr_blocks[1]
+    description_blocks = description.split('{noformat}')
+    if len(description_blocks) >= 3:
+        return description_blocks[1]
     else:
         return ''
+
+
+def matches_exception_throw_location(new_stacktrace, issue_stacktrace):
+    line_new_stacktrace = first_line_caused_by_from_printed_stacktrace(new_stacktrace)
+    line_issue_stacktrace = first_line_caused_by_from_printed_stacktrace(issue_stacktrace)
+
+    return line_new_stacktrace == line_issue_stacktrace
+
+
+def first_line_caused_by_from_printed_stacktrace(printed_stacktrace):
+    lines = printed_stacktrace.splitlines()
+    loc_last_causedby_line = -1
+    for i in range(len(lines)):
+        if REGEX_CAUSED_BY.match(lines[i]):
+            loc_last_causedby_line = i
+
+    # Split at the colon, first element of tuple contains entire string if colon not found
+    exception_line = lines[loc_last_causedby_line + 1].partition(':')
+    return exception_line[0]
 
 
 def create_details_string_from_json(json_data):
