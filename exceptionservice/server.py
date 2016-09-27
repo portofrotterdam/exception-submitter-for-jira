4418b9d6e62cdb30e51ba4d329cb02d70c565d33
Miel Donkers <miel.donkers@codecentric.nl>
2016-09-27 08:48:58 +0200

    Query and detect duplicate Jira issues

--- exceptionservice/server.py
+++ exceptionservice/server.py
@@ -1,6 +1,7 @@
 import logging
 import io
 from copy import deepcopy
+from difflib import SequenceMatcher
 from urllib.parse import urljoin
 
 import requests
@@ -47,12 +48,50 @@ def show_all_open_issues():
 
 def add_jira_exception(json_data):
     log.info('Received json data: {}'.format(json.dumps(json_data)))
-    result = add_to_jira(get_summary_from_message(json_data), create_details_string_from_json(json_data), get_stacktrace_from_message(json_data))
+    is_duplicate = determine_if_duplicate(json_data)
+    if is_duplicate[0]:
+        return 'Jira issue already exists: {}'.format(is_duplicate[1])
 
+    result = add_to_jira(get_summary_from_message(json_data), create_details_string_from_json(json_data), get_stacktrace_from_message(json_data))
     if result[0] == 201:
         return 'Jira issue added: {}'.format(result[1]['key']), 201, {}
     else:
         return 'Could not create new Jira issue, resp code; {}'.format(result[0])
+
+
+def determine_if_duplicate(json_data):
+    exception = get_summary_from_message(json_data)
+    query = {'jql': 'project=HAMISTIRF&issuetype=Bevinding&summary ~ ' + exception, 'fields': _JIRA_FIELDS}
+    resp = requests.post(_JIRA_URI_SEARCH,
+                         json=query,
+                         headers=_CONTENT_JSON_HEADER,
+                         auth=_JIRA_USER_PASSWD)
+
+    if resp.status_code != 200:
+        return True, 'Could not query Jira issues, cancel processing issue. Resp code; {}'.format(resp.status_code)
+
+    # log.info('QUERY result: \n' + json.dumps(resp.json()) + '\n\n')
+    for issue in resp.json()['issues']:
+        issue_stacktrace = get_stacktrace_from_issue(issue)
+        s = SequenceMatcher(lambda x: x == ' ' or x == '\n' or x == '\t',
+                            get_stacktrace_from_message(json_data),
+                            issue_stacktrace)
+
+        match_ratio = s.ratio() if s.real_quick_ratio() > 0.6 else 0
+        if match_ratio > 0.95:
+            log.info('\nMatch ratio: {} for stacktrace:\n{}'.format(match_ratio, issue_stacktrace))
+            return True, issue['key']
+
+    return False, ''
+
+
+def get_stacktrace_from_issue(issue):
+    description = issue['fields']['description']
+    descr_blocks = description.split('{noformat}')
+    if len(descr_blocks) >= 3:
+        return descr_blocks[1]
+    else:
+        return ''
 
 
 def create_details_string_from_json(json_data):
